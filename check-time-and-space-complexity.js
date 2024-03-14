7c331ecfa34dea8a618785324f1c71ddd40b7a1e
FaizulOsman <faizulosmanrabby@gmail.com>
2024-03-14 20:11:32 +0600

    feat: space complexity example

--- check-time-and-space-complexity.js
+++ check-time-and-space-complexity.js
@@ -24,3 +24,25 @@ console.log(sumOfAllFasterWay(1000000000));
 let time4 = performance.now();
 
 console.log(`Execution time: ${(time4 - time3) / 1000}`);
+
+/* ****************** Checking Space Complexity ****************** */
+
+/* ******** O(1) ******** */
+function sum(arr) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total; // It returns only one number
+}
+console.log(sum([1, 2, 3, 4, 5]));
+
+/* ******** O(n) ******** */
+function double(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i] * 2);
+  }
+  return newArr; // It returns an array with n times number
+}
+console.log(double([1, 2, 3, 4, 5]));
