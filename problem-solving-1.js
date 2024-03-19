5dfbc0021cc80c74a8affde21fc5b7ccf7634b43
FaizulOsman <faizulosmanrabby@gmail.com>
2024-03-19 20:38:05 +0600

    feat: Character count

--- problem-solving-1.js
+++ problem-solving-1.js
@@ -17,3 +17,18 @@ function getHigherAndLowerTemperature(arr) {
   console.log(higher, lower);
 }
 getHigherAndLowerTemperature(temperature);
+
+/* ********* Character count ********* */
+function charCount(str) {
+  const charList = {};
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i].toLowerCase();
+    if (charList[char] > 0) {
+      charList[char]++;
+    } else {
+      charList[char] = 1;
+    }
+  }
+  return charList;
+}
+console.log(charCount("Hello World"));
