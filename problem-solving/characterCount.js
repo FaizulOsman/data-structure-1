ef51a07ba997e46864d491ed0f1f9012b87b14d7
FaizulOsman <faizulosmanrabby@gmail.com>
2024-03-20 23:13:49 +0600

    feat: problem solving - character count

---
+++ problem-solving/characterCount.js
@@ -0,0 +1,14 @@
+/* ********* Character Count ********* */
+function charCount(str) {
+  const charList = {};
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i].toLowerCase();
+    if (charList[char] > 0) {
+      charList[char]++;
+    } else {
+      charList[char] = 1;
+    }
+  }
+  return charList;
+}
+console.log(charCount("Hello World"));
