4a23d1598a71380239bf62449685c834d56f9b23
FaizulOsman <faizulosmanrabby@gmail.com>
2024-03-21 21:38:37 +0600

    feat: Sum of Array using Recursion

--- recursion.js
+++ recursion.js
@@ -17,3 +17,14 @@ function sumOfNumber2(n) {
   return n + sumOfNumber2(n - 1);
 }
 console.log(sumOfNumber2(10));
+
+/* ********* Sum of Array using Recursion ********* */
+function sumOfArray(arr) {
+  if (arr.length === 0) return 0;
+
+  let rest = arr.slice(1);
+  return arr[0] + sumOfArray(rest);
+}
+// Time Complexity = O(n^2)
+// Space Complexity = O(n)
+console.log(sumOfArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
