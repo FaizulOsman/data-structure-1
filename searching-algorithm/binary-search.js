f6631e83b3286c277a71c1146a67c74b44a3f35b
FaizulOsman <faizulosmanrabby@gmail.com>
2024-04-17 09:26:58 +0600

    feat: condition addad for binary search

--- searching-algorithm/binary-search.js
+++ searching-algorithm/binary-search.js
@@ -1,3 +1,10 @@
+/* 
+
+Condition for Binary Search 
+=> The Array Should Have To In Ascending Order 
+
+*/
+
 /* Given a sorted array of 'n' elements and a target element 't', find the index of 't' in the array. Return -1 if the target element is not found. */
 
 // arr = [-5, 2, 4, 6, 10], t=10 => should return 4
